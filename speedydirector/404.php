266c79443bdb5fd48dc5420a145b6ab42d259d18
Hyppolite <hyppolitak@gmail.com>
2024-03-31 19:09:29 +0100

    Add the 404 template

---
+++ speedydirector/404.php
@@ -0,0 +1,7 @@
+<?php get_header(); ?>
+<div id="main" class="group fourohfour">
+    <h2>404 | NOT FOUND</h2>
+    <img class="aligncenter" src="<?php print IMAGES;?>/404.jpg" alt="404'd!"/>
+    <p>Oups! The page is not found. Maybe you should just move along back to the <a href="<?php bloginfo('home'); ?>">homepage</a>.</p>
+</div>
+<?php get_footer(); ?>
