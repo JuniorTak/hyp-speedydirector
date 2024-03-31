cb138af989d46d85861028f485cdf81bd13650bc
Hyppolite <hyppolitak@gmail.com>
2024-03-31 18:44:24 +0100

    Add custom functionalities

---
+++ speedydirector/functions.php
@@ -0,0 +1,30 @@
+<?php
+/* Define two constants that we’ll use in our theme */
+define( 'TEMPPATH', get_bloginfo('stylesheet_directory'));
+define( 'IMAGES', TEMPPATH. "/images");
+
+/* Add menu support */
+add_theme_support('nav-menus');
+if (function_exists('register_nav_menus')) {
+    register_nav_menus(
+        array(
+            'main' => 'Main Nav'
+        )
+    );
+}
+
+/* Add support for a sidebar */
+if (function_exists('register_sidebar')) {
+    register_sidebar(
+        array (
+            'name' => __( 'Primary Sidebar', 'primary-sidebar' ),
+            'id' => 'primary-widget-area',
+            'description' => __( 'The primary widget area', 'dir' ),
+            'before_widget' => '<div class="widget">',
+            'after_widget' => "</div>",
+            'before_title' => '<h3 class="widget-title">',
+            'after_title' => '</h3>',
+        )
+    );
+}
+?>
