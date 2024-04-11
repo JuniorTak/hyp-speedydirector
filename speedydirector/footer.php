1266fa80c51e097926284b95d52a2c09ecb63b09
Hyppolite <hyppolitak@gmail.com>
2024-04-11 01:27:14 +0100

    Correct the RSS feed link

--- speedydirector/footer.php
+++ speedydirector/footer.php
@@ -1,24 +1,32 @@
-            </div>
-            <!-- End Main Area -->
-        </div><!--end container-->
-    </div><!--end wrap-->
-    <!-- Footer Information -->
-    <footer class="group">
-        <?php
-        //social links
-        $facebook= get_option('director_facebook');
-        $twitter= get_option('director_twitter');
-        $rss= get_option('director_rss'); 
-		?>
+			</div>
+			<!-- End Main Area -->
+		</div><!--end container-->
+	</div><!--end wrap-->
+	<!-- Footer Information -->
+	<footer class="group">
+		<?php
+		// social links
+		$facebook = get_option( 'director_facebook' );
+		$twitter  = get_option( 'director_twitter' );
+		$rss      = get_option( 'director_rss' ); ?>
 		<ul class="social">
-			<?php if($facebook) : ?><li><a href="<?php print $facebook; ?>"><img src="<?=IMAGES?>/facebook.png" /></li><?php endif; ?>
-			<?php if($twitter) : ?><li><a href="<?php print $twitter; ?>"><img src="<?=IMAGES?>/twitter.png" /></li><?php endif; ?>
-			<?php if($rss) : ?><li><a href="<?php bloginfo('rss'); ?>"><img src="<?=IMAGES?>/feed.png" /></a></li><?php endif; ?>
+			<?php
+			if ( $facebook ) :
+				?>
+			<li><a href="<?php print $facebook; ?>"><img src="<?php echo IMAGES; ?>/facebook.png" /></li><?php endif; ?>
+			<?php
+			if ( $twitter ) :
+				?>
+			<li><a href="<?php print $twitter; ?>"><img src="<?php echo IMAGES; ?>/twitter.png" /></li><?php endif; ?>
+			<?php
+			if ( $rss ) :
+			?>
+			<li><a href="<?= site_url('/feed')  ?>"><img src="<?php echo IMAGES; ?>/feed.png" /></a></li><?php endif; ?>
 		</ul>
-        <p>&copy; <?php bloginfo('name'); ?>, <?=date('Y');?>. All Rights Reserved</p>
-    </footer>
-    <!-- End Footer Information -->
-    <?php wp_footer(); ?>
-    <?php print get_option('director_analytics'); ?>
+		<p>&copy; <?php bloginfo( 'name' ); ?>, <?php echo date( 'Y' ); ?>. All Rights Reserved</p>
+	</footer>
+	<!-- End Footer Information -->
+	<?php wp_footer(); ?>
+	<?php print get_option( 'director_analytics' ); ?>
 </body>
 </html>
